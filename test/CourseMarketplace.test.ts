0a9f0d5e7808c9ee5dea0a254548bfee4eef0068
hidetomo0102 <hidenorishio@icloud.com>
2022-02-11 22:06:19 +0900

    add test

--- test/CourseMarketplace.test.ts
+++ test/CourseMarketplace.test.ts
@@ -10,7 +10,7 @@ contract("CourseMarketplace", (accounts) => {
   let index = 0;
 
   describe("Purchase the new course", () => {
-    it("can get the purchased course hash by index", async () => {
+    it("Success: can get the purchased course hash by index", async () => {
       const _contract = await CourseMarketplace.deployed();
 
       await _contract.purchaseCourse(courseID, proof, { from: buyer, value });
@@ -30,7 +30,15 @@ contract("CourseMarketplace", (accounts) => {
       assert.equal(courseHash, expectedHash, "Course hash ins not");
     });
 
-    it("should match the data of the course purchased by buyer", async () => {
+    it("Fail: not allowed to purchase already owned course", async () => {
+      const _contract = await CourseMarketplace.deployed();
+      try {
+        await _contract.purchaseCourse(courseID, proof, { from: buyer, value });
+        expect.fail("repurchase should have declined");
+      } catch (err) {}
+    });
+
+    it("Success: should match the data of the course purchased by buyer", async () => {
       const _contract = await CourseMarketplace.deployed();
       const courseHash = await _contract.getCourseHashAtIndex(index);
 
@@ -60,9 +68,10 @@ contract("CourseMarketplace", (accounts) => {
   });
 
   describe("Activate the purchased course", () => {
-    it("should have 'actived' state", async () => {
+    it("Success: should have 'actived' state", async () => {
       const _contract = await CourseMarketplace.deployed();
       const courseHash = await _contract.getCourseHashAtIndex(index);
+
       await _contract.activateCourse(courseHash, { from: owner });
 
       const course = await _contract.getCourseByHash(courseHash);
@@ -72,6 +81,48 @@ contract("CourseMarketplace", (accounts) => {
         Number(course.state),
         expectedState,
         "Course state sould be 'activated'"
+      );
+    });
+
+    it("Fail: should onlyOwner modifier works", async () => {
+      const _contract = await CourseMarketplace.deployed();
+      const courseHash = await _contract.getCourseHashAtIndex(index);
+
+      try {
+        await _contract.activateCourse(courseHash, { from: buyer });
+        expect.fail("onlyOwner should have worked");
+      } catch (err) {}
+    });
+  });
+
+  describe("Transfer ownership", () => {
+    it("Success: should return deployer address", async () => {
+      const _contract = await CourseMarketplace.deployed();
+      const currentOwner = await _contract.getContractOwner();
+
+      assert.equal(owner, currentOwner, "Contract owner is not matching");
+    });
+
+    it("Fail: contract owner is wrong", async () => {
+      const _contract = await CourseMarketplace.deployed();
+
+      try {
+        await _contract.transferOwnership(accounts[3], { from: accounts[4] });
+        expect.fail("onlyOwner should have worked");
+      } catch (err) {}
+    });
+
+    it("Success: should transfer ownership to 2nd address", async () => {
+      const _contract = await CourseMarketplace.deployed();
+      const currentOwner = await _contract.getContractOwner();
+
+      await _contract.transferOwnership(accounts[2], { from: currentOwner });
+
+      const owner = await _contract.getContractOwner();
+      assert.equal(
+        owner,
+        accounts[2],
+        "Contract owner is not the second account"
       );
     });
   });
